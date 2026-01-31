bb026dfa368c4362cedb346dc1e0d252a4f4079a
Minseo Park <studio61315@gmail.com>
2026-01-31 15:13:04 +0900

    build: drop pixi support

--- development/scripts/pixi/activation.bat
+++
@@ -1,8 +0,0 @@
-:: Find Python executable path for nanobind and Boost.Python
-for /f "tokens=*" %%i in ('python -c "import sys; print(sys.executable)"') do set PYTHON_EXECUTABLE=%%i
-
-:: Set default build value only if not previously set
-if not defined COAL_BUILD_TYPE (set COAL_BUILD_TYPE=Release)
-if not defined COAL_PYTHON_STUBS (set COAL_PYTHON_STUBS=ON)
-if not defined COAL_PYTHON_NANOBIND (set COAL_PYTHON_NANOBIND=ON)
-if not defined COAL_HAS_QHULL (set COAL_HAS_QHULL=OFF)
